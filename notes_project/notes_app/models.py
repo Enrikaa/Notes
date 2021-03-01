5b49eb50c8c60d570d1a5973111f00dc84f77982
aurimas <aurimas.butk@gmail.com>
2021-03-01 14:59:09 +0200

    update

--- notes_project/notes_app/models.py
+++ notes_project/notes_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Note_model(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=150,unique=True)
+    title = models.CharField(max_length=100,unique=True)
+    note_text = models.CharField(max_length=20000)
