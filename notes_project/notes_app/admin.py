2e4015ad785902911fb3187e35b43d470aebea28
Enrikaa <enrika.vysniauskaitee@gmail.com>
2021-03-02 18:00:24 +0200

    Update admin

--- notes_project/notes_app/admin.py
+++ notes_project/notes_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from notes_app.models import Note_model
+from notes_app.models import Note_model, Topic, Test
 # Register your models here.
 
 admin.site.register(Note_model)
+admin.site.register(Topic)
+admin.site.register(Test)
--- notes_project/notes_app/models.py
+++ notes_project/notes_app/models.py
@@ -7,5 +7,15 @@ class Note_model(models.Model):
     title = models.CharField(max_length=100,unique=True)
     note_text = models.CharField(max_length=20000)
 
-    def __str__(self):
-        return self.title
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=150,unique=True)
+    title = models.CharField(max_length=100,unique=True)
+    text = models.CharField(max_length=20000)
+
+class Test(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=150,unique=True)
+    title = models.CharField(max_length=100,unique=True)
+    text = models.CharField(max_length=20000)
+
