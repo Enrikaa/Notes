1f0be71184fa2b00a30727a72f56f1c816d73a87
aurimas <aurimas.butk@gmail.com>
2021-03-04 22:00:35 +0200

    update

--- notes_project/notes_app/forms.py
+++ notes_project/notes_app/forms.py
@@ -1,25 +1,26 @@
 from .models import Note_model
-from django.forms import ModelForm,TextInput
+from django.forms import ModelForm, TextInput
 from django import forms
 from django.core.exceptions import ValidationError
+
 
 class Noteform(forms.Form):
     category = forms.CharField(max_length=264)
     title = forms.CharField(max_length=264)
     note_text = forms.CharField(max_length=264)
 
-    category.widget.attrs.update({'class':'form-control'})
-    title.widget.attrs.update({'class':'form-control'})
-    note_text.widget.attrs.update({'class':'form-control'})
-    
+    category.widget.attrs.update({'class': 'form-control'})
+    title.widget.attrs.update({'class': 'form-control'})
+    note_text.widget.attrs.update({'class': 'form-control'})
+
     def clean_title(self):
-        new_title=self.cleaned_data['title'].lower()
-        if new_title=='create':
+        new_title = self.cleaned_data['title'].lower()
+        if new_title == 'create':
             raise ValidationError('Title may not be "Create"')
         return new_title
 
     def save(self):
-        new_note=Note_model.objects.create(
+        new_note = Note_model.objects.create(
             category=self.cleaned_data['category'],
             title=self.cleaned_data['title'],
             note_text=self.cleaned_data['note_text'],
--- notes_project/notes_app/views.py
+++ notes_project/notes_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from notes_app.forms import Noteform
 from django import forms
@@ -6,27 +6,33 @@ from urllib import request
 from notes_app.models import Note_model
 # Create your views here.
 
+
 def index(request):
-    return render(request,'notes_app/index.html')
+    return render(request, 'notes_app/index.html')
+
 
 def create(request):
-    bound_form=Noteform(request.POST)
+    bound_form = Noteform(request.POST)
 
     if bound_form.is_valid():
-        new_form=bound_form.save()
+        new_form = bound_form.save()
         return redirect('index')
-        
-    return render(request,'notes_app/create.html',{'form':bound_form})   
+
+    return render(request, 'notes_app/create.html', {'form': bound_form})
+
 
 def read(request):
-    data=Note_model.objects.all()
-    return render(request, 'notes_app/read.html',{'notes':data})
+    data = Note_model.objects.all()
+    return render(request, 'notes_app/read.html', {'notes': data})
+
 
 def undo(request):
-    return render(request,'notes_app/undo.html')
+    note = Noteform(request.GET)
+    return render(request, 'notes_app/undo.html', {'note': note})
+
 
 def delete(request):
     posts = Note_model.objects.all()
 
     args = {'posts': posts}
-    return render(request,'notes_app/delete.html', context=args)
+    return render(request, 'notes_app/delete.html', context=args)
