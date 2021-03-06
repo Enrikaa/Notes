76487030d783728a1cd7b1a9c38fcbe05f8ff2e5
aurimas <aurimas.butk@gmail.com>
2021-03-06 22:41:45 +0200

    create account model

--- notes_project/notes_app/admin.py
+++ notes_project/notes_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from notes_app.models import Note_model
+from notes_app.models import Note_model, Account
 # Register your models here.
 
 admin.site.register(Note_model)
-
+admin.site.register(Account)
