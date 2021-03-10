31c681034fa3a9af79d7ff2351ba303c5e78cf30
Enrikaa <enrika.vysniauskaitee@gmail.com>
2021-03-10 13:14:56 +0200

    Logout button and function

--- notes_project/notes_app/views.py
+++ notes_project/notes_app/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import forms
 from urllib import request
-from notes_app.forms import UserInfoForm, Accountform
+from notes_app.forms import UserInfoForm
 
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from notes_app.forms import UserForm, UserProfileInfoForm
 
 from django.views import View
 
@@ -17,13 +18,13 @@ def index(request):
 
 
 def create(request):
-    bound_form = Noteform(request.POST)
+    # bound_form = Noteform(request.POST)
 
-    if bound_form.is_valid():
-        new_form = bound_form.save()
-        return redirect('index')
+    # if bound_form.is_valid():
+    #     # new_form = bound_form.save()
+    #     return redirect('index')
 
-    return render(request, 'notes_app/create.html', {'form': bound_form})
+    return render(request, 'notes_app/create.html')
 
 
 def read(request):
@@ -31,8 +32,8 @@ def read(request):
 
 
 def undo(request):
-    note = Noteform(request.GET)
-    return render(request, 'notes_app/undo.html', {'note': note})
+    # note = Noteform(request.GET)
+    return render(request, 'notes_app/undo.html')
 
 
 def delete(request):
@@ -53,7 +54,7 @@ def test_update(request, num):
 
 def update(request, num):
     # model = Note_model.objects.get(id=num)
-    forma = Noteform()
+    # form = Noteform()
     # context = {'form': model}
     return render(request, 'notes_app/update.html')
 
@@ -77,14 +78,61 @@ def userinfo(request):
     return render(request, 'notes_app/userinfo.html', {'user_info': user_info, 'registered': registered})
 
 
+# def register(request):
+#     bound_form = Accountform(request.POST)
+
+#     if bound_form.is_valid():
+#         _form = bound_form.save_account()
+#         return redirect('index')
+
+#     return render(request, 'notes_app/register.html', {'form': bound_form})
+
 def register(request):
-    bound_form = Accountform(request.POST)
+    registered = False
 
-    if bound_form.is_valid():
-        _form = bound_form.save_account()
-        return redirect('index')
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
 
-    return render(request, 'notes_app/register.html', {'form': bound_form})
+        if user_form.is_valid() and profile_form.is_valid():
+
+            #Grabing the user form and saving it to DB
+            user = user_form.save()
+            #Hashing the pasword
+            user.set_password(user.password)
+            user.save()
+
+            #Don't commit to DB yet
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+                profile.save()
+
+                registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'notes_app/register.html', 
+                            {'user_form':user_form,
+                            'profile_form': profile_form,
+                            'registered': registered})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+# Return something special for user
+@login_required
+def special(request):
+    return HttpResponse("You are logged in, Nice!")
+
 
 def user_login(request):
 
